0ba5508547c4024faf19314d123217d1e22897fb
Amarikwa <amarikwa188@gmail.com>
2024-08-17 15:23:36 +0100

    game settings setup

---
+++ game_settings.py
@@ -0,0 +1,4 @@
+class Settings:
+    def __init__(self) -> None:
+        self.screen_width: int = 900
+        self.screen_height: int  = 600
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import pygame
+from pygame import Surface
+
+from game_settings import Settings
 
 
 def run_game() -> None:
-    pass
+    pygame.init()
+    settings: Settings = Settings()
+    screen: Surface = pygame.display.set_mode((settings.screen_width,
+                                               settings.screen_height))
+    
+    while True:
+        pass
 
 
 if __name__ == "__main__":
