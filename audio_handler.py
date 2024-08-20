59ee0cda97c6112d7315b7d4372ecd843e1b5daa
Amarikwa <amarikwa188@gmail.com>
2024-08-20 20:31:12 +0100

    added background music

--- audio_handler.py
+++ audio_handler.py
@@ -1,8 +1,17 @@
+from pygame import mixer
 from pygame.mixer import Sound
 
 
 class AudioHandler:
     def __init__(self) -> None:
+        # background music
+        mixer.init()
+        mixer.music.load("audio_files/background.mp3")
+        mixer.music.set_volume(0.2)
+        mixer.music.play(-1)
+
+
+        # sound effects
         self.fruit_sound: Sound = Sound("audio_files/score.mp3")
         self.move_sound: Sound = Sound("audio_files/slither.mp3")
         self.lose_sound: Sound = Sound("audio_files/lose.mp3")
